292fe2f5c85dafb4bffaeedb8c8d9acaa712aa1a
AhmedxSaid <ahmedsaid.ect@gmail.com>
2025-02-28 17:11:22 +0100

    feature:add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,16 @@
 <template>
   <v-app>
     <v-main>
-      <router-view />
-    </v-main>
+        <NavBar />
+        <router-view />
+      </v-main>
+      
+  
   </v-app>
 </template>
 
 <script setup>
-  //
+  
+import NavBar from './components/Navbar.vue';
+
 </script>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template template>
+  <div>
+    <v-navigation-drawer v-model="drawer" app class="indigo">
+      <v-toolbar-title>My App</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn text>Home</v-btn>
+    <v-btn text>About</v-btn>
+    <v-btn text>Contact</v-btn>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+export default{
+Data(){
+    return{
+        drawer: true
+    }
+}
+}
+</script>
+
+<style></style>
